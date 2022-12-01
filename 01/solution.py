200e97d83ca3902a415cbf3490e8e57b7a706091
Vaclav Lunak <v.lunak@gmail.com>
2022-12-01 10:49:07 +0100

    day 1, global runner

---
+++ 01/solution.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def update_max(max_elfs, curr_elf):
+    for i in range(len(max_elfs)):
+        if curr_elf > max_elfs[i]:
+            return max_elfs[:i] + [curr_elf] + max_elfs[i+1:]
+
+    return max_elfs
+
+try:
+    max_elfs = [0, 0, 0]
+    curr_elf = 0
+    while True:
+        line = input()
+        if line == '':
+            max_elfs = update_max(max_elfs, curr_elf)
+            curr_elf = 0
+        else:
+            curr_elf += int(line)
+except EOFError:
+    print(max_elfs)
+    print(sum(max_elfs))
+
+
