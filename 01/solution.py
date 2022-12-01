53776a06f4010b7647712fc67cd6d562d29688f9
Vaclav Lunak <v.lunak@gmail.com>
2022-12-01 10:59:42 +0100

    reformatted day 1 solution

--- 01/solution.py
+++ 01/solution.py
@@ -4,16 +4,17 @@
 def update_max(max_elfs, curr_elf):
     for i in range(len(max_elfs)):
         if curr_elf > max_elfs[i]:
-            return max_elfs[:i] + [curr_elf] + max_elfs[i+1:]
+            return max_elfs[:i] + [curr_elf] + max_elfs[i + 1 :]
 
     return max_elfs
+
 
 try:
     max_elfs = [0, 0, 0]
     curr_elf = 0
     while True:
         line = input()
-        if line == '':
+        if line == "":
             max_elfs = update_max(max_elfs, curr_elf)
             curr_elf = 0
         else:
@@ -21,5 +22,3 @@ try:
 except EOFError:
     print(max_elfs)
     print(sum(max_elfs))
-
-
